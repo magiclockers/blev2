4ea27b92bc22d09dd2b52df5327102b0337c530e
izumi <kinizumi@gmail.com>
2021-10-10 16:04:16 -0400

    initiate plugin

---
+++ src/definitions.ts
@@ -0,0 +1,11 @@
+declare module '@capacitor/core' {
+  interface PluginRegistry {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    ContactsPlugin: ContactsPluginPlugin;
+  }
+}
+
+export interface ContactsPluginPlugin {
+  echo(options: { value: string }): Promise<{ value: string }>;
+  getContacts(filter: string): Promise<{results: any[]}>;
+}
