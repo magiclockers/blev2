ba46820cee2beb57f8f17dff54eb98470dd3a748
izumi <kinizumi@gmail.com>
2021-10-11 01:31:33 -0400

    test case without crash

--- android/src/main/java/com/magiclockers/contacts/ContactsPlugin.java
+++ android/src/main/java/com/magiclockers/contacts/ContactsPlugin.java
@@ -1,14 +1,14 @@
 package com.magiclockers.contacts;
 
-import android.Manifest;
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.provider.ContactsContract;
 import android.util.Log;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+//import androidx.appcompat.app.AppCompatActivity;
+import com.inuker.bluetooth.library.connect.listener.BleConnectStatusListener;
 
 import com.airbnk.sdk.callback.IConnectDeviceCallback;
 import com.airbnk.sdk.callback.IDeviceStatusCallback;
@@ -22,41 +22,43 @@ import com.airbnk.sdk.MainApi;
 
 import org.json.JSONArray;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-class Api extends AppCompatActivity {
+class Api extends Activity {
+  private final BleConnectStatusListener mConnectStatusListener = new BleConnectStatusListener() {
+    @Override
+    public void onConnectStatusChanged(String mac, int status) {
+    }
+  };
 }
 
 @NativePlugin(
   requestCodes = {ContactsPlugin.REQUEST_CONTACTS}
 )
 public class ContactsPlugin extends Plugin {
-  protected static final int REQUEST_CONTACTS = 22345; // Unique request code
+  protected static final int REQUEST_CONTACTS = 12345; // Unique request code
   private com.airbnk.sdk.MainApi mainApi;
+  public String value = "init";
 
   @PluginMethod()
   public void echo(PluginCall call) {
-    String value = call.getString("value");
-
-    JSObject ret = new JSObject();
-    ret.put("value", "ayaya");
-    call.resolve(ret);
-  }
-  @PluginMethod()
-  public void getContacts(PluginCall call) {
-    String value = call.getString("filter");
-    // Filter based on the value if want
-    //test
+    value = call.getString("value");
+//test
     Api api = new Api();
-    mainApi = new MainApi(api, "aaa", "bbb");
+    mainApi = new MainApi(api, "12345678901234567890123456789012", "bbbbbb");
+  //  try {
+
     mainApi.connect(new IConnectDeviceCallback() {
       @Override
       public void onSuccess() {
-        mainApi.unlock("aaa", new IUnlockCallback() {
+        value = "connect succeed";
+        mainApi.unlock("12345678901234567890123456789012", new IUnlockCallback() {
           @Override
           public void onSuccess() {
+            value = "unlock succeed";
             if (mainApi != null) {
               mainApi.disconnect();
             }
@@ -64,24 +66,40 @@ public class ContactsPlugin extends Plugin {
 
           @Override
           public void onFailed(String errorMsg) {
+            value = errorMsg;
           }
         });
       }
 
       @Override
       public void onFailed(String errorMsg) {
-
+        value = errorMsg;
       }
     }, new IDeviceStatusCallback() {
       @Override
       public void states(int i) {
-
+        value = "connect failed";
       }
     });
+     ////m.invoke(testObject);
+//    } catch (Exception e) {
+//      e.getCause();
+//        e.printStackTrace();
+//    }
+
     //test
     JSObject ret = new JSObject();
-    ret.put("result.firstName", "ayaya");
-    ret.put("result.lastName", "ffff");
+    ret.put("value", value);
+    call.success(ret);
+  }
+  @PluginMethod()
+  public void getContacts(PluginCall call) {
+    String value = call.getString("filter");
+    // Filter based on the value if want
+    JSObject ret = new JSObject();
+    ret.put("firstName", "ayaya");
+    ret.put("lastName", "ffff");
+    ret.put("telephone", "ffff");
     call.success(ret);
 
    // saveCall(call);
