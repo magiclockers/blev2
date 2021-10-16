b1c41f5c4b2b00a3507768144c7f388c913d6ffa
izumi <kinizumi@gmail.com>
2021-10-16 17:20:36 -0400

    pass sninfo from plugin class to sdk activity class

--- android/src/main/java/com/magiclockers/contacts/Api.java
+++ android/src/main/java/com/magiclockers/contacts/Api.java
@@ -1,34 +1,25 @@
 package com.magiclockers.contacts;
-
 import android.app.Activity;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
+import android.os.Looper;
 
 import com.airbnk.sdk.MainApi;
 import com.airbnk.sdk.callback.IConnectDeviceCallback;
 import com.airbnk.sdk.callback.IDeviceStatusCallback;
 import com.airbnk.sdk.callback.IUnlockCallback;
-import com.inuker.bluetooth.library.connect.listener.BleConnectStatusListener;
 
 public class Api extends Activity {
   private MainApi mainApi;
   public String value = "init";
-  String sn = "TiHeSBp9BlLKFW6jgfondDy9ovkoUGwbsFLES3HYkRPQ48KL5aZe9dMtLCS/I44M/wVZWV0KUqUdJT1oq9FR//8FWVldClKlHSU9aKvRUf//BVlZXQpSpR0lPWir0VH//wVZWV0KUqUdJT1oq9FR//igVOBiVnz7W2LScEbB3TdUWjQH+gTO5sfaexY2pOf+39SoNpk07DhbbEfkoqemCgrwp++6XTAcupECUJbt9NgQ4qZlVxTNm7OgQ8TBSIt71KxQfNH0iMUHJ0KRMwBVXlysgpW7Tzt+sl3GoX3zF6n/BVlZXQpSpR0lPWir0VH//wVZWV0KUqUdJT1oq9FR//8FWVldClKlHSU9aKvRUf//BVlZXQpSpR0lPWir0VH//wVZWV0KUqUdJT1oq9FR//8FWVldClKlHSU9aKvRUf//BVlZXQpSpR0lPWir0VH//wVZWV0KUqUdJT1oq9FR//8FWVldClKlHSU9aKvRUf//BVlZXQpSpR0lPWir0VH//wVZWV0KUqUdJT1oq9FR//8FWVldClKlHSU9aKvRUf//BVlZXQpSpR0lPWir0VH//wVZWV0KUqUdJT1oq9FR//8FWVldClKlHSU9aKvRUf//BVlZXQpSpR0lPWir0VH//wVZWV0KUqUdJT1oq9FR/+i/cVq1OcLiYGSr/pfPSVQ=a8677206db3ee35f1c4a1700eaa2c8d7";
   String key = "airbnkHaha123456";
-  public SQLiteDatabase db;
-  public final BleConnectStatusListener mConnectStatusListener = new BleConnectStatusListener() {
-    @Override
-    public void onConnectStatusChanged(String mac, int status) {
-    }
-  };
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Bundle extras = getIntent().getExtras();
+    //if (extras != null) {
+    final String sn = extras.getString("sn");
+    //}
     try {
       mainApi = new MainApi(Api.this, sn, key);
       if (mainApi != null) {
@@ -73,8 +64,24 @@ public class Api extends Activity {
           }
         });
       }
+      ///
+      new android.os.Handler(Looper.getMainLooper()).postDelayed(
+        new Runnable() {
+          public void run() {
+            Api.this.finish();
+            if (mainApi != null) {
+              mainApi.disconnect();
+            }
+          }
+        },
+        7000);//force close activity and connection after a while
+      ///
 
     } catch (Exception e) {
+      Api.this.finish();
+      if (mainApi != null) {
+        mainApi.disconnect();
+      }
       e.getCause();
       e.printStackTrace();
     }
--- android/src/main/java/com/magiclockers/contacts/ContactsPlugin.java
+++ android/src/main/java/com/magiclockers/contacts/ContactsPlugin.java
@@ -1,35 +1,19 @@
 package com.magiclockers.contacts;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
 
-import com.airbnk.sdk.callback.IGetDynamicPasswordCallback;
-import com.inuker.bluetooth.library.connect.listener.BleConnectStatusListener;
 
-import com.airbnk.sdk.callback.IConnectDeviceCallback;
-import com.airbnk.sdk.callback.IDeviceStatusCallback;
-import com.airbnk.sdk.callback.IUnlockCallback;
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+
+import com.airbnk.sdk.callback.IGetDynamicPasswordCallback;
 import com.airbnk.sdk.MainApi;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import static androidx.core.content.ContextCompat.startActivity;
-
-class Aapi extends Activity {
+class ApiPass extends Activity {
 }
 
 @NativePlugin(
@@ -45,9 +29,11 @@ public class ContactsPlugin extends Plugin {
   String key = "airbnkHaha123456";
 
   @PluginMethod()
-  public void echo(PluginCall call) {
+  public void echo(PluginCall call) {// connect bluetooth and unlock
     value = call.getString("value");
     Intent intent = new Intent(this.getContext(), new Api().getClass());
+    intent.putExtra("sn", sn);
+
     getActivity().startActivity(intent);
     JSObject ret = new JSObject();
     ret.put("value", value);
@@ -55,9 +41,9 @@ public class ContactsPlugin extends Plugin {
   }
 
   @PluginMethod()
-  public void getContacts(PluginCall call) {
+  public void getContacts(PluginCall call) {// get password
     value = call.getString("value");
-    Aapi api = new Aapi();
+    ApiPass api = new ApiPass();
     mainApi = new MainApi(api, sn, key);
     mainApi.getDynamicPassword(sn, new IGetDynamicPasswordCallback() {
       @Override
